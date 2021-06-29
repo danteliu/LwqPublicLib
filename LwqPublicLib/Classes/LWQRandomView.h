e8a11d8e841c421bd94fa4cb2f493107dd3cd2fa
凝智电脑 <1849245841@qq.com>
2021-06-29 09:08:27 +0800

    提交第四个版本测试

---
+++ LwqPublicLib/Classes/LWQRandomView.h
@@ -0,0 +1,17 @@
+//
+//  LWQRandomView.h
+//  LwqPublicLib_Example
+//
+//  Created by ric on 2021/6/28.
+//  Copyright © 2021 1849245841@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <NerdyUI/NerdyUI.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LWQRandomView : UIView
+-(instancetype)initWithFrame:(CGRect)frame;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LwqPublicLib/Classes/LWQRandomView.m
@@ -0,0 +1,22 @@
+//
+//  LWQRandomView.m
+//  LwqPublicLib_Example
+//
+//  Created by ric on 2021/6/28.
+//  Copyright © 2021 1849245841@qq.com. All rights reserved.
+//
+
+#import "LWQRandomView.h"
+
+@implementation LWQRandomView
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.bgColor(@"random");
+    }
+    return self;
+}
+
+
+@end
